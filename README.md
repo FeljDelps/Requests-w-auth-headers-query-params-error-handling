4376b1ec35353c7a66fe5ded740ecc1170561ad4
Felj Abas <felabas41@gmail.com>
2020-10-14 20:43:48 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Your team is working on an app that will help recruiters review all of a candidate's information. You've been assigned to work on one feature for the app - to display a list of repos belonging to a particular GitHub handle.
---
+++ index.js
@@ -0,0 +1,46 @@
+
+function displayResults(jsonResponse){
+    console.log('displayResults ran');
+    for (let i=0; i < jsonResponse.length; i++){
+     $('#results-list').append(`<li>
+     <h3><a href= '${jsonResponse[i].html_url}'>${jsonResponse[i].name}</a></h3>
+     </li>`
+     );};
+};
+
+
+function getRepos(githubHandle){
+    let url = `https://api.github.com/users/${githubHandle}/repos`; 
+    console.log ('getRepos ran ' + url);
+    
+
+    fetch(url)
+    .then(response => {
+        if(response.ok){
+            console.log('fetch API ran')
+            return response.json();
+        }
+        throw new Error(response.status);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+        $('#js-error-message').text(`Something went wrong`);
+        console.log(err.message)
+    })
+
+}
+
+
+function watchform(){
+    $('#js-form').submit(event => {
+        event.preventDefault();
+        console.log('watchForm ran');
+        let userHandle = $('#js-submission').val();
+        console.log(userHandle);
+        getRepos(userHandle);
+    });
+}
+
+
+
+$(watchform)
